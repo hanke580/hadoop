b62b79dcad706da7d2e54ac7c8b3887ee6b9f415
Ke Han <kehan5800@gmail.com>
2024-04-11 21:04:57 -0600

    inst

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/util/LightWeightResizableGSet.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/util/LightWeightResizableGSet.java
@@ -38,94 +38,93 @@ import org.apache.hadoop.classification.InterfaceAudience;
  *       (2) implementing {@link LinkedElement} interface.
  */
 @InterfaceAudience.Private
-public class LightWeightResizableGSet<K, E extends K>
-    extends LightWeightGSet<K, E> {
+public class LightWeightResizableGSet<K, E extends K> extends LightWeightGSet<K, E> {
 
-  /**
-   * The default initial capacity - MUST be a power of two.
-   */
-  static final int DEFAULT_INITIAL_CAPACITY = 1 << 4;
+    /**
+     * The default initial capacity - MUST be a power of two.
+     */
+    static final int DEFAULT_INITIAL_CAPACITY = 1 << 4;
 
-  /**
-   * The load factor used when none specified in constructor.
-   */
-  static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    /**
+     * The load factor used when none specified in constructor.
+     */
+    static final float DEFAULT_LOAD_FACTOR = 0.75f;
 
-  /** Size of the entry table. */
-  private int capacity;
+    /**
+     * Size of the entry table.
+     */
+    private int capacity;
 
-  /**
-   * The load factor for the hash set.
-   */
-  private final float loadFactor;
-  private int threshold;
+    /**
+     * The load factor for the hash set.
+     */
+    private final float loadFactor;
 
-  public LightWeightResizableGSet(int initCapacity, float loadFactor) {
-    if (initCapacity < 0) {
-      throw new HadoopIllegalArgumentException("Illegal initial capacity: " +
-          initCapacity);
+    private int threshold;
+
+    public LightWeightResizableGSet(int initCapacity, float loadFactor) {
+        if (initCapacity < 0) {
+            throw new HadoopIllegalArgumentException("Illegal initial capacity: " + initCapacity);
+        }
+        if (loadFactor <= 0 || loadFactor > 1.0f) {
+            throw new HadoopIllegalArgumentException("Illegal load factor: " + loadFactor);
+        }
+        this.capacity = actualArrayLength(initCapacity);
+        this.hash_mask = capacity - 1;
+        this.loadFactor = loadFactor;
+        this.threshold = (int) (capacity * loadFactor);
+        entries = new LinkedElement[capacity];
     }
-    if (loadFactor <= 0 || loadFactor > 1.0f) {
-      throw new HadoopIllegalArgumentException("Illegal load factor: " +
-          loadFactor);
+
+    public LightWeightResizableGSet() {
+        this(DEFAULT_INITIAL_CAPACITY, DEFAULT_LOAD_FACTOR);
     }
-    this.capacity = actualArrayLength(initCapacity);
-    this.hash_mask = capacity - 1;
-    this.loadFactor = loadFactor;
-    this.threshold = (int) (capacity * loadFactor);
 
-    entries = new LinkedElement[capacity];
-  }
-
-  public LightWeightResizableGSet() {
-    this(DEFAULT_INITIAL_CAPACITY, DEFAULT_LOAD_FACTOR);
-  }
-
-  public LightWeightResizableGSet(int initCapacity) {
-    this(initCapacity, DEFAULT_LOAD_FACTOR);
-  }
-
-  @Override
-  public E put(final E element) {
-    E existing = super.put(element);
-    expandIfNecessary();
-    return existing;
-  }
-
-  /**
-   * Resize the internal table to given capacity.
-   *
-   * @param cap capacity.
-   */
-  @SuppressWarnings("unchecked")
-  protected void resize(int cap) {
-    int newCapacity = actualArrayLength(cap);
-    if (newCapacity == this.capacity) {
-      return;
+    public LightWeightResizableGSet(int initCapacity) {
+        this(initCapacity, DEFAULT_LOAD_FACTOR);
     }
-    this.capacity = newCapacity;
-    this.threshold = (int) (capacity * loadFactor);
-    this.hash_mask = capacity - 1;
-    LinkedElement[] oldEntries = entries;
-    entries = new LinkedElement[capacity];
-    for (int i = 0; i < oldEntries.length; i++) {
-      LinkedElement e = oldEntries[i];
-      while (e != null) {
-        LinkedElement next = e.getNext();
-        int index = getIndex((E)e);
-        e.setNext(entries[index]);
-        entries[index] = e;
-        e = next;
-      }
-    }
-  }
 
-  /**
-   * Checks if we need to expand, and expands if necessary.
-   */
-  protected void expandIfNecessary() {
-    if (size > this.threshold && capacity < MAX_ARRAY_LENGTH) {
-      resize(capacity * 2);
+    @Override
+    public E put(final E element) {
+        E existing = super.put(element);
+        expandIfNecessary();
+        return existing;
     }
-  }
+
+    /**
+     * Resize the internal table to given capacity.
+     *
+     * @param cap capacity.
+     */
+    @SuppressWarnings("unchecked")
+    protected void resize(int cap) {
+        int newCapacity = actualArrayLength(cap);
+        if (newCapacity == this.capacity) {
+            return;
+        }
+        this.capacity = newCapacity;
+        this.threshold = (int) (capacity * loadFactor);
+        this.hash_mask = capacity - 1;
+        LinkedElement[] oldEntries = entries;
+        entries = new LinkedElement[capacity];
+        for (int i = 0; i < oldEntries.length; i++) {
+            LinkedElement e = oldEntries[i];
+            while (e != null) {
+                LinkedElement next = e.getNext();
+                int index = getIndex((E) e);
+                e.setNext(entries[index]);
+                entries[index] = e;
+                e = next;
+            }
+        }
+    }
+
+    /**
+     * Checks if we need to expand, and expands if necessary.
+     */
+    protected void expandIfNecessary() {
+        if ((org.zlab.ocov.tracker.Runtime.updateBranch(size > this.threshold && capacity < MAX_ARRAY_LENGTH, 46))) {
+            resize(capacity * 2);
+        }
+    }
 }
--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/EditLogBackupOutputStream.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/EditLogBackupOutputStream.java
@@ -20,7 +20,6 @@ package org.apache.hadoop.hdfs.server.namenode;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.Arrays;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.apache.hadoop.hdfs.HdfsConfiguration;
@@ -37,120 +36,128 @@ import org.apache.hadoop.security.UserGroupInformation;
 /**
  * An implementation of the abstract class {@link EditLogOutputStream},
  * which streams edits to a backup node.
- * 
+ *
  * @see org.apache.hadoop.hdfs.server.protocol.NamenodeProtocol#journal
  * (org.apache.hadoop.hdfs.server.protocol.NamenodeRegistration,
  *  int, int, byte[])
  */
 class EditLogBackupOutputStream extends EditLogOutputStream {
-  private static final Logger LOG =
-      LoggerFactory.getLogger(EditLogFileOutputStream.class);
-  static final int DEFAULT_BUFFER_SIZE = 256;
 
-  private final JournalProtocol backupNode;  // RPC proxy to backup node
-  private final NamenodeRegistration bnRegistration;  // backup node registration
-  private final JournalInfo journalInfo;  // active node registration
-  private final DataOutputBuffer out;     // serialized output sent to backup node
-  private EditsDoubleBuffer doubleBuf;
+    private static final Logger LOG = LoggerFactory.getLogger(EditLogFileOutputStream.class);
 
-  EditLogBackupOutputStream(NamenodeRegistration bnReg, // backup node
-      JournalInfo journalInfo, int logVersion) // active name-node
-      throws IOException {
-    super();
-    this.bnRegistration = bnReg;
-    this.journalInfo = journalInfo;
-    InetSocketAddress bnAddress =
-      NetUtils.createSocketAddr(bnRegistration.getAddress());
-    try {
-      this.backupNode = NameNodeProxies.createNonHAProxy(new HdfsConfiguration(),
-          bnAddress, JournalProtocol.class, UserGroupInformation.getCurrentUser(),
-          true).getProxy();
-    } catch(IOException e) {
-      Storage.LOG.error("Error connecting to: " + bnAddress, e);
-      throw e;
-    }
-    this.doubleBuf = new EditsDoubleBuffer(DEFAULT_BUFFER_SIZE);
-    this.out = new DataOutputBuffer(DEFAULT_BUFFER_SIZE);
-    setCurrentLogVersion(logVersion);
-  }
-  
-  @Override // EditLogOutputStream
-  public void write(FSEditLogOp op) throws IOException {
-    doubleBuf.writeOp(op, getCurrentLogVersion());
- }
+    static final int DEFAULT_BUFFER_SIZE = 256;
 
-  @Override
-  public void writeRaw(byte[] bytes, int offset, int length) throws IOException {
-    throw new IOException("Not supported");
-  }
+    // RPC proxy to backup node
+    private final JournalProtocol backupNode;
 
-  /**
-   * There is no persistent storage. Just clear the buffers.
-   */
-  @Override // EditLogOutputStream
-  public void create(int layoutVersion) throws IOException {
-    assert doubleBuf.isFlushed() : "previous data is not flushed yet";
-    this.doubleBuf = new EditsDoubleBuffer(DEFAULT_BUFFER_SIZE);
-    setCurrentLogVersion(layoutVersion);
-  }
+    // backup node registration
+    private final NamenodeRegistration bnRegistration;
 
-  @Override // EditLogOutputStream
-  public void close() throws IOException {
-    // close should have been called after all pending transactions 
-    // have been flushed & synced.
-    int size = doubleBuf.countBufferedBytes();
-    if (size != 0) {
-      throw new IOException("BackupEditStream has " + size +
-                          " records still to be flushed and cannot be closed.");
-    } 
-    RPC.stopProxy(backupNode); // stop the RPC threads
-    doubleBuf.close();
-    doubleBuf = null;
-  }
+    // active node registration
+    private final JournalInfo journalInfo;
 
-  @Override
-  public void abort() throws IOException {
-    RPC.stopProxy(backupNode);
-    doubleBuf = null;
-  }
+    // serialized output sent to backup node
+    private final DataOutputBuffer out;
 
-  @Override // EditLogOutputStream
-  public void setReadyToFlush() throws IOException {
-    doubleBuf.setReadyToFlush();
-  }
+    private EditsDoubleBuffer doubleBuf;
 
-  @Override // EditLogOutputStream
-  protected void flushAndSync(boolean durable) throws IOException {
-    assert out.getLength() == 0 : "Output buffer is not empty";
-    
-    if (doubleBuf.isFlushed()) {
-      LOG.info("Nothing to flush");
-      return;
+    // backup node
+    EditLogBackupOutputStream(// backup node
+    NamenodeRegistration bnReg, // active name-node
+    JournalInfo journalInfo, // active name-node
+    int logVersion) throws IOException {
+        super();
+        this.bnRegistration = bnReg;
+        this.journalInfo = journalInfo;
+        InetSocketAddress bnAddress = NetUtils.createSocketAddr(bnRegistration.getAddress());
+        try {
+            this.backupNode = NameNodeProxies.createNonHAProxy(new HdfsConfiguration(), bnAddress, JournalProtocol.class, UserGroupInformation.getCurrentUser(), true).getProxy();
+        } catch (IOException e) {
+            Storage.LOG.error("Error connecting to: " + bnAddress, e);
+            throw e;
+        }
+        this.doubleBuf = new EditsDoubleBuffer(DEFAULT_BUFFER_SIZE);
+        this.out = new DataOutputBuffer(DEFAULT_BUFFER_SIZE);
+        setCurrentLogVersion(logVersion);
     }
 
-    int numReadyTxns = doubleBuf.countReadyTxns();
-    long firstTxToFlush = doubleBuf.getFirstReadyTxId();
-    
-    doubleBuf.flushTo(out);
-    if (out.getLength() > 0) {
-      assert numReadyTxns > 0;
-      
-      byte[] data = Arrays.copyOf(out.getData(), out.getLength());
-      out.reset();
-      assert out.getLength() == 0 : "Output buffer is not empty";
-
-      backupNode.journal(journalInfo, 0, firstTxToFlush, numReadyTxns, data);
+    // EditLogOutputStream
+    @Override
+    public void write(FSEditLogOp op) throws IOException {
+        doubleBuf.writeOp(op, getCurrentLogVersion());
     }
-  }
 
-  /**
-   * Get backup node registration.
-   */
-  NamenodeRegistration getRegistration() {
-    return bnRegistration;
-  }
+    @Override
+    public void writeRaw(byte[] bytes, int offset, int length) throws IOException {
+        throw new IOException("Not supported");
+    }
 
-  void startLogSegment(long txId) throws IOException {
-    backupNode.startLogSegment(journalInfo, 0, txId);
-  }
+    /**
+     * There is no persistent storage. Just clear the buffers.
+     */
+    // EditLogOutputStream
+    @Override
+    public void create(int layoutVersion) throws IOException {
+        assert doubleBuf.isFlushed() : "previous data is not flushed yet";
+        this.doubleBuf = new EditsDoubleBuffer(DEFAULT_BUFFER_SIZE);
+        setCurrentLogVersion(layoutVersion);
+    }
+
+    // EditLogOutputStream
+    @Override
+    public void close() throws IOException {
+        // close should have been called after all pending transactions
+        // have been flushed & synced.
+        int size = doubleBuf.countBufferedBytes();
+        if (size != 0) {
+            throw new IOException("BackupEditStream has " + size + " records still to be flushed and cannot be closed.");
+        }
+        // stop the RPC threads
+        RPC.stopProxy(backupNode);
+        doubleBuf.close();
+        doubleBuf = null;
+    }
+
+    @Override
+    public void abort() throws IOException {
+        RPC.stopProxy(backupNode);
+        doubleBuf = null;
+    }
+
+    // EditLogOutputStream
+    @Override
+    public void setReadyToFlush() throws IOException {
+        doubleBuf.setReadyToFlush();
+    }
+
+    // EditLogOutputStream
+    @Override
+    protected void flushAndSync(boolean durable) throws IOException {
+        assert out.getLength() == 0 : "Output buffer is not empty";
+        if (doubleBuf.isFlushed()) {
+            LOG.info("Nothing to flush");
+            return;
+        }
+        int numReadyTxns = doubleBuf.countReadyTxns();
+        long firstTxToFlush = doubleBuf.getFirstReadyTxId();
+        doubleBuf.flushTo(out);
+        if (out.getLength() > 0) {
+            assert numReadyTxns > 0;
+            byte[] data = Arrays.copyOf(out.getData(), out.getLength());
+            out.reset();
+            assert out.getLength() == 0 : "Output buffer is not empty";
+            backupNode.journal(journalInfo, 0, firstTxToFlush, numReadyTxns, data);
+        }
+    }
+
+    /**
+     * Get backup node registration.
+     */
+    NamenodeRegistration getRegistration() {
+        return bnRegistration;
+    }
+
+    void startLogSegment(long txId) throws IOException {
+        backupNode.startLogSegment(journalInfo, 0, txId);
+    }
 }
